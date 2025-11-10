67bbd77a38eaea2f69fa041ff936d4601c3be290
Nick Cabahug <nickjoey.cabahug@msunaawan.edu.ph>
2025-11-10 22:29:06 +0800

    Code for navigation bar - Nick Joey Cabahug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import SearchFilters from "./components/navbar/SearchFilters";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+
+        <div className="pt-32">
+          {children}
+        </div>
+        </body>
     </html>
   );
 }

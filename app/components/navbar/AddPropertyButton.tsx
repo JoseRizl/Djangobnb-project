6fa580b0d1ba20cac55d6224ace4a63b19bae180
Nick Cabahug <nickjoey.cabahug@msunaawan.edu.ph>
2025-11-27 11:48:53 +0800

    Add properties from nextjs: Create modal - Nick Joey Cabahug

--- app/components/navbar/AddPropertyButton.tsx
+++ app/components/navbar/AddPropertyButton.tsx
@@ -1,6 +1,32 @@
-const AddPropertyButton = () => {
+'use client';
+
+import useAddPropertyModal from "@/app/hooks/useAddPropertyModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import LoginModal from "../modals/LoginModal";
+
+interface AddPropertyButtonProps {
+    userId?: string | null;
+}
+
+const AddPropertyButton: React.FC<AddPropertyButtonProps> = ({ 
+    userId 
+}) => {
+    const loginModal = useLoginModal();
+    const AddPropertyModal = useAddPropertyModal();
+
+    const airbnbYourHome = () => {
+        if (userId) {
+            AddPropertyModal.open();
+        } else {
+            loginModal.open();
+        }
+
+    }
     return (
-        <div className="cursor-pointer p-2 text-sm font-semibold rounded-full hover:bg-gray-200">
+        <div 
+            onClick={airbnbYourHome}
+            className="cursor-pointer p-2 text-sm font-semibold rounded-full hover:bg-gray-200"
+        >
             Djangobnb your home
         </div>
     )
